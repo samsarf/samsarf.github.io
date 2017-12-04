c2fa9c922de12d8981a37fbd9e3742fab21397d1
Sameera Sarfaraz <sameera.sarfaraz@yahoo.com>
2017-12-03 19:10:52 -0500

    minified the js

--- samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/grunt/Gruntfile.js
+++ samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/grunt/Gruntfile.js
@@ -3,6 +3,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    cssmin : {
+      target : {
+        src : "/views/css/bootstrap-grid.css",
+        dest : "/views/css/bootstrap-grid.min.css"
+      }
+    }
     uglify:{
       build:{
         src: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/js/main.js",
@@ -11,9 +17,14 @@ module.exports = function(grunt) {
     }
   });
 
+  //load cssmin plugin, uglify
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  
 
+  //create default task
+  grunt.registerTask('default', ['cssmin']);
   grunt.registerTask('default', ['uglify']);
 
 };
+
+  
