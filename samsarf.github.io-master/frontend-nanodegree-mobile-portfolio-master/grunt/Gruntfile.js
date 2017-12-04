d7050bf2c25e83f3af2e19a0a09e1c5da3f2dd18
Sameera Sarfaraz <sameera.sarfaraz@yahoo.com>
2017-12-04 11:34:47 -0500

    Initial Commit

--- samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/grunt/Gruntfile.js
+++ samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/grunt/Gruntfile.js
@@ -1,30 +1,29 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    cssmin : {
-      target : {
-        src : "/views/css/bootstrap-grid.css",
-        dest : "/views/css/bootstrap-grid.min.css"
-      }
-    }
-    uglify:{
-      build:{
-        src: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/js/main.js",
-        dest: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/js/main.min.js"
-      }
-    }
-  });
+       cssmin: {
+            target: {
+                src: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/css/bootstrap-grid.css",
+                dest: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/css/bootstrap-grid.min.css"
+            }
+        }
+        uglify: {
+            build: {
+                src: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/js/main.js",
+                dest: "/Users/sameerasarfaraz/Desktop/samsarf.github.io-master/frontend-nanodegree-mobile-portfolio-master/views/js/main.min.js"
+            }
+        }
+    });
 
-  //load cssmin plugin, uglify
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    //load cssmin plugin, uglify 
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  //create default task
-  grunt.registerTask('default', ['cssmin']);
-  grunt.registerTask('default', ['uglify']);
+    //create default task 
+    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['uglify']);
 
 };
-
   
